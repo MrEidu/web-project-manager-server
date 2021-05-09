1f0e3ae5c5359f0be161ee1a52b4a06c58ad2c1b
Victor <a320663@uach.mx>
2021-05-09 12:08:28 -0600

    fixed some typos

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(cors());
 
 
 app.use('/projects', projectsRouter);
-app.use('/column', columnsRouter);
+app.use('/column', columnRouter);
 
 
 // catch 404 and forward to error handler
--- routes/column.js
+++ routes/column.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/columns')
+const controller = require('../controllers/column')
 
 router.get('/:page?', controller.list);
 
